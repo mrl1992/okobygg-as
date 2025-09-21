4c38615290287c5dc57baaf44c0a3a11da55f1d2
Markus Lippestad <ml@altere.no>
2025-09-21 20:37:56 +0200

    sanity deployment app id

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,7 +3,10 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'o7kte07u',
-    dataset: 'production'
+    dataset: 'production',
+  },
+  deployment: {
+    appId: 's3578r5mmr0zz33gam5hg3qc',
   },
   /**
    * Enable auto-updates for studios.
